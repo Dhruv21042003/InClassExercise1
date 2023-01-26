1ef1e23b2a81a6b5050b01a64b91ffb966d1b3b7
Dhruv21042003 <pate4235@sheridancollege.ca>
2023-01-25 19:07:45 -0500

    This is a Branch Code!

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,12 +1,14 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Dhruv
+ * @author Dhruv Patel Jan 25, 2023 
  */
 public class CardTrick {
     
@@ -16,13 +18,31 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            card.setValue((int) (1 + Math.random() * 13));
+            card.setSuit(Card.SUITS[(int)(0 + Math.random() * 3)]);
+            hand[i] = card;
+            System.out.println(hand[i].getSuit() + " " + hand[i].getValue());
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Suit: ");
+        String usersuit = input.nextLine();
+        System.out.print("Enter Card Value: ");
+        int uservalue = input.nextInt();
+        
+        for(int i=0; i<hand.length; i++)
+        {
+            if(usersuit.equalsIgnoreCase(hand[i].getSuit()) && uservalue==hand[i].getValue())
+            {
+                printInfo();
+                break;
+            }
+        }
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
@@ -38,7 +58,7 @@ public class CardTrick {
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Dhruv Patel Jan 25, 2022
      */
     private static void printInfo() {
     
